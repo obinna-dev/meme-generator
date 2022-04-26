04ebec17ab74883409f35af6936c14f504a7f51f
iamthefastone <o.osisiogu@gmail.com>
2022-04-26 08:59:06 +0100

    Clean up code so far

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -30,14 +30,3 @@ export default function Meme()  {
         </main>
     )
 }
-
-    /**
-     * Challenge: Save the random meme URL in state
-     * - Create new state called `memeImage` with an
-     *   empty string as default
-     * - When the getMemeImage function is called, update
-     *   the `memeImage` state to be the random chosen
-     *   image URL
-     * - Below the div.form, add an <img /> and set the
-     *   src to the new `memeImage` state you created
-     */
